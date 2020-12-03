db967a362356c2ec5a973599979fea00baac7523
ms-online <27732357@qq.com>
2020-12-03 16:47:23 +0800

    lesson-2

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-lesson-1:初始化 APP
+lesson-2:创建 Header & Footer 组件
+
+### React-bootstrap 链接：
+
+https://react-bootstrap.netlify.app/components/list-group/#list-group-props
+
+### font-awesome 链接：
+
+https://cdnjs.com/libraries/font-awesome
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Hello World</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
