9e9535ad3aeb9d380708a68face5fbd188545535
ms-online <27732357@qq.com>
2020-12-10 11:36:28 +0800

    lesson-66

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -36,4 +36,62 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 })
 
-export { getProducts, getProductById, deleteProduct }
+//@desc    创建产品
+//@route   POST/api/products
+//@access  私密（仅限管理员）
+const createProduct = asyncHandler(async (req, res) => {
+  //创建一个产品模版
+  const product = new Product({
+    name: '样品名称',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: '样品品牌',
+    category: '样品分类',
+    countInStock: 0,
+    numReviews: 0,
+    description: '样品描述',
+    rating: 0,
+  })
+  const createdProduct = await product.save()
+  res.status(201).json(createdProduct)
+})
+
+//@desc    更新产品内容
+//@route   PUT/api/products/:id
+//@access  私密（仅限管理员）
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body
+
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    product.name = name
+    product.price = price
+    product.description = description
+    product.image = image
+    product.brand = brand
+    product.category = category
+    product.countInStock = countInStock
+    const updatedProduct = await product.save()
+    res.status(201).json(updatedProduct)
+  } else {
+    res.status(404)
+    throw new Error('查询不到产品')
+  }
+})
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,11 +3,17 @@ import {
   getProducts,
   getProductById,
   deleteProduct,
+  createProduct,
+  updateProduct,
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
-router.route('/').get(getProducts)
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
 
 export default router
